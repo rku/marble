138b18b90251cbf19cd634e893d9bc6270c84def
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-04-15 20:25:19 +0200

    wip idis

--- src/lib/MarbleModel.cpp
+++ src/lib/MarbleModel.cpp
@@ -263,7 +263,8 @@ void MarbleModel::setMapThemeId( const QString &mapThemeId )
     QList<GeoSceneGeodata> currentDatasets;
     if ( d->m_mapTheme ) {
         foreach ( GeoSceneLayer *layer, d->m_mapTheme->map()->layers() ) {
-            if ( layer->backend() != dgml::dgmlValue_geodata )
+            if ( layer->backend() != dgml::dgmlValue_geodata
+                 && layer->backend() != dgml::dgmlValue_vector )
                 continue;
 
             // look for documents
@@ -313,7 +314,8 @@ void MarbleModel::setMapThemeId( const QString &mapThemeId )
     QList<GeoDataStyle*> styleList;
 
     foreach ( GeoSceneLayer *layer, d->m_mapTheme->map()->layers() ) {
-        if ( layer->backend() != dgml::dgmlValue_geodata )
+        if ( layer->backend() != dgml::dgmlValue_geodata
+             && layer->backend() != dgml::dgmlValue_vector )
             continue;
 
         GeoSceneGeodata emptyData("empty");
